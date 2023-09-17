6c016ac96b80342ad1b138afe4abc0942468426b
Adebayo Temitope <tsadebayo@myseneca.ca>
2023-09-16 22:47:48 -0400

    completed

---
+++ File.h
@@ -0,0 +1,21 @@
+/*
+  Name: Temitope Oladimeji Samuel Adebayo
+
+  I have done all the coding by myself and only copied the code that my professor 
+  provided to complete my workshops and assignments.
+*/
+
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+
+#include "ShoppingRec.h"
+
+namespace sdds {
+  bool openFileForRead();
+  bool openFileForOverwrite();
+  void closeFile();
+  bool freadShoppingRec(ShoppingRec* rec);
+  void fwriteShoppintRec(const ShoppingRec* rec);
+}
+
+#endif
---
+++ Utils.h
@@ -0,0 +1,19 @@
+/*
+  Name: Temitope Oladimeji Samuel Adebayo
+
+  I have done all the coding by myself and only copied the code that my professor 
+  provided to complete my workshops and assignments.
+*/
+
+#ifndef SDDS_UTILS_H
+#define SDDS_UTILS_H
+
+namespace sdds {
+  void flushkeys();
+  bool ValidYesResponse(char ch);
+  bool yes();
+  void readCstr(char cstr[], int len);
+  int readInt(int min, int max);
+}
+
+#endif
